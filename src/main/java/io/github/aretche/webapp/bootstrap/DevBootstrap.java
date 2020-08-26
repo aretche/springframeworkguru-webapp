33109dd3e2ddda2e58c95423b345196c2d92af48
arellanog <arellanog@cent.gov.ar>
2020-08-26 10:53:49 -0300

    Agrego bootstap para cargar datos de prueba

---
+++ src/main/java/io/github/aretche/webapp/bootstrap/DevBootstrap.java
@@ -0,0 +1,45 @@
+package io.github.aretche.webapp.bootstrap;
+
+import io.github.aretche.webapp.model.*;
+import io.github.aretche.webapp.repositories.*;
+import org.springframework.context.*;
+import org.springframework.context.event.*;
+import org.springframework.stereotype.*;
+
+@Component
+public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+        initData();
+    }
+
+    private void initData(){
+
+        Author eric = new Author("Eric", "Evans");
+        Book  ddd = new Book("Domain Driven Design", "1234", "Harper Collins");
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "23444", "Wrox" );
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+    }
+
+}
